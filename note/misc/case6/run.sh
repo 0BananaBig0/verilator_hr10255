c40e545dac498e8f7da5fa207e590384d31a093b
Banana-Two <1184903633@qq.com>
2022-05-02 13:25:57 +0800

    format case

--- note/misc/case6/run.sh
+++ note/misc/case6/run.sh
@@ -5,6 +5,6 @@
 # Created Time: Sun 05 Dec 2021 04:58:54 PM CST
 #########################################################################
 #!/bin/bash
-# ~/Downloads/program/verilator/bin/verilator -Wno-implicit ripple_adder_oasys_2021_h.syn.v ../LibBlackbox.v --xml-output netlist_h.xml
-~/cpp_workspace/verilator_hr10255/bin/verilator -Wno-implicit ripple_adder_oasys_2021_h.syn.v ../LibBlackbox.v --xml-only
+# ~/Downloads/program/verilator/bin/verilator -Wno-implicit ripple_adder_oasys_2021_h_modified.syn.v ../LibBlackbox.v --xml-output netlist_h.xml
+~/cpp_workspace/verilator_hr10255/bin/verilator -Wno-implicit ripple_adder_oasys_2021_h_modified.syn.v ../LibBlackbox.v --xml-only
 # /home/hxliang/Downloads/program/verilator/bin/verilator_bin -Wno-implicit ripple_adder_oasys_2021_h.syn.v ../LibBlackbox.v --xml-output netlist_nh.xml --flatten
