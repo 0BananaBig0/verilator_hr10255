e0dd362d7d46edd54b180abbe983ceba1eef15c7
Huaxiao Liang <1184903633@qq.com>
2022-05-31 09:55:43 +0800

    add scripts to update hcstest

---
+++ update_hcstest.sh
@@ -0,0 +1,7 @@
+current_path=`pwd`
+cd src
+cp MultipleBitsNetlist.h ~/cpp_workspace/hcstest.devel/verilog-parser/verilator-parser/
+cp OneBitHierNetlist.cpp ~/cpp_workspace/hcstest.devel/verilog-parser/verilator-parser/
+cp OneBitHierNetlist.h ~/cpp_workspace/hcstest.devel/verilog-parser/verilator-parser/
+cp OneBitNetlist.h ~/cpp_workspace/hcstest.devel/verilog-parser/verilator-parser/
+cp VerilogNetlist.cpp ~/cpp_workspace/hcstest.devel/verilog-parser/verilator-parser/
