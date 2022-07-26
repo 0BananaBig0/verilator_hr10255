62d756ed76c54184c31c450b18ed62c7e9f56b48
Huaxiao Liang <1184903633@qq.com>
2022-07-27 00:08:37 +0800

    update case19 about inout

--- note/misc/case19/typical_example_many_inout.v
+++ note/misc/case19/typical_example_many_inout.v
@@ -20,7 +20,7 @@ module typical_example(inout0,inout1,inout2,input3,input4,input5,input6,
    output output15;
    output output16;
    output output17;
-   output [4:0]output18;
+   output [10:0]output18;
    wire [13:0]wire19;
   // Top port is input connecting to an ins inout: the ins inout is used as
   // input.
@@ -28,7 +28,7 @@ module typical_example(inout0,inout1,inout2,input3,input4,input5,input6,
 
   // Top port is output connecting to an ins inout: the ins inout is used as
   // output.
-  PADBID PADBID_U0 (.PAD(output14), .I(input3), .OEN(input4), .C(wire19[0]));
+  PADBID PADBID_U0 (.PAD(output18[5]), .I(input3), .OEN(input4), .C(wire19[0]));
 
   // Top port is inout connecting to an ins inout and an ins input: the ins
   // inout and top inout are both used as inout.
@@ -43,21 +43,23 @@ module typical_example(inout0,inout1,inout2,input3,input4,input5,input6,
   PADBID PADBID_U5 (.PAD(inout2[1]), .I(input3), .OEN(input4), .C());
   // Top port is inout connecting to an ins inout and an ins output: the ins
   // inout is used as input and the top inout is used as inout.
-  PADBID PADBID_U6 (.PAD(inout2[2]), .I(input3), .OEN(input4), .C(inout2[3]));
+  PADBID PADBID_U6 (.PAD(), .I(input3), .OEN(input4), .C(inout2[3]));
   PADBID PADBID_U7 (.PAD(inout2[3]), .I(input3), .OEN(input4), .C());
 
   // Top port is wire connection to an ins inout and an ins input: the ins
   // inout is used as output.
   PADBID PADBID_U8 (.PAD(wire19[6]), .I(input3), .OEN(input4), .C(wire19[5]));
-  PADBID PADBID_U9 (.PAD(wire19[7]), .I(wire19[6]), .OEN(input4), .C(wire19[4]));
+  PADBID PADBID_U9 (.PAD(), .I(wire19[6]), .OEN(input4), .C(wire19[4]));
   // Top port is wire connecting to an ins inout and an ins output: the ins
   // inout is used as input.
-  PADBID PADBID_U10 (.PAD(wire19[1]), .I(input3), .OEN(input4), .C(wire19[3]));
+  PADBID PADBID_U10 (.PAD(), .I(input3), .OEN(input4), .C(wire19[3]));
   PADBID PADBID_U11 (.PAD(wire19[3]), .I(input3), .OEN(input4), .C(wire19[8]));
   // Top port is wire connecting to two ins inouts: both ins
   // inouts are used as inout.
   PADBID PADBID_U12 (.PAD(wire19[9]), .I(input3), .OEN(input4), .C(wire19[13]));
   PADBID PADBID_U13 (.PAD(wire19[9]), .I(input3), .OEN(input4), .C(wire19[13]));
+  // Top port is wire connecting to an ins inout: the ins inout is used as
+  // inout
   PADBID PADBID_U14 (.PAD(wire19[10]), .I(input3), .OEN(input4), .C(wire19[13]));
   DFFRS_X1 DFFRS_X1_U15 (.CK(input6), .D(input7), .RN(input8), .SN(input9), .Q(output14), .QN(output15));
   FA_X1 FA_X1_U16 (.A(input3), .B(input4), .CI(input5), .CO(output16), .S(output17));
